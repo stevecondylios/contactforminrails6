be13ec85666ab20665cfd0890af7d8be808f18aa
Steve Condylios <steve.condylios@gmail.com>
2021-07-31 17:46:44 +1000

    First

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,22 @@
+class ContactsController < ApplicationController
+
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:error] = nil
+    else
+      flash.now[:error] = 'Could not send message'
+      render :new
+    end
+  end
+
+
+
+
+end
